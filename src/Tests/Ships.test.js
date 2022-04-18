646fddb2f2a4c928eaac2498f2e83f259d9d281b
Hassane24 <hsisane5@gmail.com>
2022-04-18 18:56:28 +0100

    finished test number one

--- src/Tests/Ships.test.js
+++ src/Tests/Ships.test.js
@@ -1,3 +1,13 @@
 import { ships } from "../Modules/Ships";
 
+test("Check the whole object", () => {
+  expect(ships("carrier", [1, 5, 6])).toEqual({
+    sunken: false,
+    name: "carrier",
+    position: [1, 5, 6],
+    hits: [],
+    length: 3,
+  });
+});
+
 test("", () => {});
